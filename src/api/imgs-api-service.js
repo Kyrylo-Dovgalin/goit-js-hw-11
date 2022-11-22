072c759a1381b3f02cac3839b7b9db93c98f0fcf
Kyrylo Dovgalin <kyrylo.dovgalin@gmail.com>
2022-11-22 13:38:11 +0200

    version#1

--- src/api/imgs-api-service.js
+++ src/api/imgs-api-service.js
@@ -1,3 +1,22 @@
+const BASE_URL = 'https://pixabay.com/api/';
+
 const options = {
   KEY: '31423107-978e712529a799d220018245f',
+  imageType: 'photo',
+  orientation: 'horizontal',
+  safeSearch: 'true',
+  perPage: 40,
 };
+
+function fetchImages(query, page) {
+  return fetch(
+    `${BASE_URL}?key=${options.KEY}&q=${query}&image_type=${options.imageType}&orientation=${options.orientation}&safesearch=${options.safeSearch}&per_page=${options.perPage}&page=${page}`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+export { fetchImages };
--- src/index.js
+++ src/index.js
@@ -1,30 +1,113 @@
 import axios from 'axios';
-// all modules
-import Notiflix from 'notiflix';
-// // one by one
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import { Report } from 'notiflix/build/notiflix-report-aio';
-// import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
-// import { Loading } from 'notiflix/build/notiflix-loading-aio';
-// import { Block } from 'notiflix/build/notiflix-block-aio';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import './css/styles.css';
+import { fetchImages } from './api/imgs-api-service';
 
 const refs = {
   form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+let searchQuery = null;
+let page = null;
 
 function onFormSubmit(e) {
+  page = 1;
   e.preventDefault();
+  clearMarkUp();
+  refs.loadMoreBtn.classList.add('is-hidden');
   //   const formData = new FormData(e.currentTarget);
   //   const searchQuery = formData.get('searchQuery');
-  const searchQuery = e.currentTarget.elements.searchQuery.value.trim();
+  searchQuery = e.currentTarget.elements.searchQuery.value.trim();
+  console.log(searchQuery);
   if (!searchQuery) {
-    Notiflix.Notify.info('Empty request');
+    Notify.info('Empty request, please type not only spaces');
     return;
   }
-  Notiflix.Notify.success(searchQuery);
+  fetchImages(searchQuery, page)
+    .then(images => {
+      if (!images.hits.length) {
+        Notify.info(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+      // for (const image of images.hits) {
+      createImagesMarkUp(images.hits);
+      refs.loadMoreBtn.classList.remove('is-hidden');
+      // }
+
+      console.log(images);
+      Notify.success(
+        `We found ${images.totalHits} , buy licence to get more, total found ${images.total}`
+      );
+    })
+    .catch(error => Notify.failure(`${error}`));
+}
+
+function createImagesMarkUp(imagesData) {
+  const markup = imagesData
+    .map(image => {
+      return `<div class="photo-card">
+  <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${image.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${image.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${image.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${image.downloads}</b>
+    </p>
+  </div>
+</div>`;
+    })
+    .join(' ');
+
+  //   webformatURL - ссылка на маленькое изображение для списка карточек.
+  // largeImageURL - ссылка на большое изображение.
+  // tags - строка с описанием изображения. Подойдет для атрибута alt.
+  // likes - количество лайков.
+  // views - количество просмотров.
+  // comments - количество комментариев.
+  // downloads - количество загрузок.
+
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function clearMarkUp() {
+  refs.gallery.innerHTML = ``;
+}
+
+function onLoadMore(e) {
+  page += 1;
+  fetchImages(searchQuery, page)
+    .then(images => {
+      if (!images.hits.length) {
+        Notify.info(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+      // for (const image of images.hits) {
+      createImagesMarkUp(images.hits);
+      refs.loadMoreBtn.classList.remove('is-hidden');
+      // }
+
+      console.log(images);
+      Notify.success(
+        `We found ${images.totalHits} , buy licence to get more, total found ${images.total}`
+      );
+    })
+    .catch(error => Notify.failure(`${error}`));
 }
